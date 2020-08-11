40e496ce2277eff3e086ad1cce6462f88ca4c5d7
Valjacques <adrien.pouemo@nzhinusoft.com>
2020-08-11 22:47:01 +0100

    final project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project 5: Serverless Application
+
+Program: Cloud Developer Nanodegree
+
+Module: Develop & Deploy Serverless App
+
+Project: Serverless Application
+
+Student: Nikolaos Alexandros Kaloumenos
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'golazz87b2'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-3.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-gcmvaxkn.us.auth0.com', // Auth0 domain
+  clientId: 'EJAH0V5TNJk37rnvfwubKUKSDpf7vMje', // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
